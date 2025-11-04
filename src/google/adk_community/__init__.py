f52ca0685cf18d5c5cfb42cf4aed81666604553e
Xuan Yang <xygoogle@google.com>
2025-11-04 10:40:35 -0800

    Update __init__.py to import version from version.py

--- src/google/adk_community/__init__.py
+++ src/google/adk_community/__init__.py
@@ -12,5 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# version: major.minor.patch
-__version__ = "0.1.0"
+from . import version
+__version__ = version.__version__
